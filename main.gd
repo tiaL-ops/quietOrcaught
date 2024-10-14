b551f46d4a3a9bb94344089cad8745d8f85045b4
tiaL-ops <104815994+tiaL-ops@users.noreply.github.com>
2024-10-14 07:55:14 -0400

    Can move and ANimated

--- main.gd
+++ main.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 # Speed of the player
 @export var speed: int = 80
 
-
+# Physics process to handle movement
 func _physics_process(delta: float) -> void:
 	# Input vector for movement
 	var input_vector = Vector2.ZERO
@@ -18,3 +18,16 @@ func _physics_process(delta: float) -> void:
 	# Move the character using move_and_slide()
 	velocity = input_vector * speed
 	move_and_slide()
+
+	# Play the correct animation based on movement direction
+	if input_vector.x > 0:
+		$AnimationPlayer.play("walk_right")
+	elif input_vector.x < 0:
+		$AnimationPlayer.play("left_walk")
+	elif input_vector.y > 0:
+		$AnimationPlayer.play("forward")
+	elif input_vector.y < 0:
+		$AnimationPlayer.play("backward")
+	else:
+		
+		$AnimationPlayer.stop()
